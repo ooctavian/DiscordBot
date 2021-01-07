72ad91ed8a5485fc950649f2278452561ae3dd92
Virgin Führer <octaviancir@gmail.com>
2021-01-07 12:52:09 +0000

    Added help command

--- commands/init.lua
+++ commands/init.lua
@@ -3,4 +3,23 @@ commands['poll'] = require('./poll')
 commands['avy'] = require('./avy')
 commands['choose'] = require('./choose')
 commands['info'] = require('./info')
+commands['help'] =
+{
+	name = 'help',
+	description = 'Get list of all the commands and what they do',
+	command = function(message)
+		local mess = ''
+		for  k,v in pairs(commands) do
+		 mess = mess .. '`' .. commands[k].name .. '` - ' .. commands[k].description .. '\n'
+		end
+		message:reply({
+			embed = {
+				title = "List of commands",
+				description = mess,
+				color = 0x81a1c1 -- hex color code
+			}})
+
+	end
+
+}
 return commands
